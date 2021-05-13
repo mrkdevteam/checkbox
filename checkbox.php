74693465ed60d727315ed73ba3e91fb7b1dd44d6
SavelievMiron <SavelievMiron@github.com>
2021-05-13 16:52:57 +0300

    changed plugin version from 0.3.1 to 0.3.2

--- checkbox.php
+++ checkbox.php
@@ -4,7 +4,7 @@
  * Plugin Name: Checkbox Integration
  * Plugin URI: https://morkva.co.ua/shop-2/checkbox?utm_source=checkbox-plugin
  * Description: WooCommerce Checkbox Integration
- * Version: 0.3.1
+ * Version: 0.3.2
  * Tested up to: 5.6
  * Requires at least: 5.2
  * Requires PHP: 5.6
