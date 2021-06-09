b82ab84bd1f40400d7317586421c1aa5dd652bfb
SavelievMiron <SavelievMiron@github.com>
2021-06-09 15:29:32 +0300

    added new options to delete func

--- uninstall.php
+++ uninstall.php
@@ -5,8 +5,17 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	die;
 }
 
-$options = array( 'ppo_login', 'ppo_password', 'ppo_cashbox_key' );
-
+$options = array(
+    'ppo_login',
+    'ppo_password',
+    'ppo_cashier_name',
+    'ppo_cashier_surname',
+    'ppo_cashbox_key',
+    'ppo_auto_create',
+    'ppo_payment_type',
+    'ppo_connected',
+    'ppo_autoopen_shift'
+);
 
 foreach ( $options as $option ) {
 	delete_option( $option_name );
