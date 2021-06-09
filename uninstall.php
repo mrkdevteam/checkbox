5f2d6f35a6d3b50beec83148ceef25ce3415fc53
SavelievMiron <SavelievMiron@github.com>
2021-06-09 15:26:55 +0300

    changed

--- uninstall.php
+++ uninstall.php
@@ -1,15 +1,13 @@
 <?php
 
-
 // if uninstall.php is not called by WordPress, die
-if (!defined('WP_UNINSTALL_PLUGIN')) {
-die;
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	die;
 }
 
-$options = ['ppo_login','ppo_password','ppo_cashbox_key'];
+$options = array( 'ppo_login', 'ppo_password', 'ppo_cashbox_key' );
 
 
-foreach ($options as $option) {
-    delete_option($option_name);
+foreach ( $options as $option ) {
+	delete_option( $option_name );
 }
-
