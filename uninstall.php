69b8adbc84ae866a37d2877cc48da33115f112f7
SavelievMiron <savelevmiron@gmail.com>
2021-10-02 14:58:33 +0300

    add new option to delete loop

--- uninstall.php
+++ uninstall.php
@@ -5,19 +5,22 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	die;
 }
 
+// delete plugin options
 $options = array(
     'ppo_login',
     'ppo_password',
     'ppo_cashier_name',
     'ppo_cashier_surname',
     'ppo_cashbox_key',
-    'ppo_auto_create',
+    'ppo_autocreate',
     'ppo_payment_type',
     'ppo_connected',
     'ppo_autoopen_shift',
-    'ppo_skip_receipt_creation'
+    'ppo_skip_receipt_creation',
+    'ppo_is_dev_mode',
+    'ppo_autocreate_receipt_order_statuses',
 );
 
 foreach ( $options as $option ) {
-	delete_option( $option_name );
+	delete_option( $option );
 }
